253abf9777942f284a182cca1a94ebd72a28e578
dotdevlarry <lbagobo@gbox.ncf.edu.ph>
2026-05-06 19:54:29 +0800

    Update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res){
 app.use('/api/users', authRoutes);
 app.use('/api/items', itemRoutes);
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
